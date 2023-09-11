be56d7590c9660bb06fe7692606eded6b147d82f
Shivam Patel <67837595+shivam531@users.noreply.github.com>
2023-09-11 08:51:53 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all the JS code as a newer Version 
+//  alert(3+3)   // we can't use in nodejs only use in browser
+/* Data types 
+   number= 2 power 53
+   bigint= 
+    string = " " 
+    boolean = true/ false 
+    null = stand alone value
+    undifined ;
+    symbol => find uniquenes of the tool 
+    
+    
+
+    obect= 
+
+*/
+
+   console.log(typeof null) // object
+   console.log(typeof undefined) // undefined
