56985ad96ca7f00cb2b46cf154ae8057e54b60b4
Shivam Patel <67837595+shivam531@users.noreply.github.com>
2023-09-11 08:19:03 +0000

    Veribale exercise

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let  accountEmail = "shivam@gmail.com"
+var accountPassword = "12345"
+accountCity = "amritsar"
+let accountState;
+
+// accountId = 2  can't change the const veriable 
+
+accountEmail = "sc@gmail.com"
+/* prefer don't use the var variable becouse of the problem in scope statement and functional statement*/
+accountPassword = 12345
+accountCity = "mumbai"
+// console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
