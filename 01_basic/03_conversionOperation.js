408591c84eb90ad85dcb4767c6c4444e9fff97c4
Shivam Patel <67837595+shivam531@users.noreply.github.com>
2023-09-12 18:54:08 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "shivam"
+// if double qutes "" value is a string 
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+// not an number (NaN)
+// "33" => "33"
+// "33abc" => NaN
+// true => 1, false =>0
+// null =>0
+// undefind => NaN
+// string =>Nan
+
+let isloggedin = 0
+let boolean = Boolean(isloggedin)
+// console.log(boolean)
+
+// 1 =>true 
+// "" => false
+// "shivam" => true
+
+let someNumber = 33
+
+ let stringnumber = String(someNumber)
+ console.log(stringnumber)
+ console.log(typeof stringnumber)
