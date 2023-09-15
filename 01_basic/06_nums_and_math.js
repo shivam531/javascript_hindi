53a39d120ac2fc90fd8104d9f4170c8eb90816ce
Shivam Patel <67837595+shivam531@users.noreply.github.com>
2023-09-15 18:28:26 +0000

    math_and_num

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,32 @@
+// const score = 400 
+// console.log(score);
+// const blance = new Number(100)
+// console.log(blance);
+
+// console.log(blance.toString().length);
+// console.log(blance.toFixed(3));
+
+// const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// // +++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4 , 3 , 6 , 8));
+// console.log(Math.min(4 , 3 , 6 , 8));
+
+console.log(Math.random());
+console.log((Math.random()*10 ) + 1);
+
+const min = 0
+const max = 36
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min )
